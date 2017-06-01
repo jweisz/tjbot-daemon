d687764fc403073878a7da33308e188598413f29
Justin Weisz <jweisz@us.ibm.com>
2017-06-01 09:21:25 -0400

    added copyright notices, cleaned up require statements

--- service-command/service-command.js
+++ service-command/service-command.js
@@ -1,13 +1,29 @@
-var util = require('util');
+/**
+ * Copyright 2017 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const util = require('util');
 const winston = require('winston');
 
-var bleno = require('bleno');
-var BlenoPrimaryService = bleno.PrimaryService;
+const bleno = require('bleno');
+const BlenoPrimaryService = bleno.PrimaryService;
 
-var CommandCharacteristic = require('./characteristic-command');
-var RequestCharacteristic = require('./characteristic-request');
-var ResponseCharacteristic = require('./characteristic-response');
-var ListenCharacteristic = require('./characteristic-listen');
+const CommandCharacteristic = require('./characteristic-command');
+const RequestCharacteristic = require('./characteristic-request');
+const ResponseCharacteristic = require('./characteristic-response');
+const ListenCharacteristic = require('./characteristic-listen');
 
 function CommandService(tjbot, name) {
     var commandCharacteristic = new CommandCharacteristic(tjbot, this);
